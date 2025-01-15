159b1b2540d6d8ab2691c27c5b4b3b70f69192be
Olivér Falvai <ofalvai@gmail.com>
2025-01-15 15:38:04 +0100

    Extract waiting logic to `go-android`

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"os"
-	"strings"
 	"time"
 
 	"github.com/bitrise-io/go-android/v2/adbmanager"
@@ -16,18 +14,8 @@ import (
 	"github.com/bitrise-io/go-utils/v2/system"
 )
 
-// Clock ...
-type Clock interface {
-	Now() time.Time
-	Since(t time.Time) time.Duration
-	Sleep(d time.Duration)
-	After(d time.Duration) <-chan time.Time
-}
-
-var clock Clock = defaultClock{}
 var logger = log.NewLogger()
 
-// Inputs ...
 type Inputs struct {
 	EmulatorSerial string `env:"emulator_serial,required"`
 	BootTimeout    int    `env:"boot_timeout,required"`
@@ -47,75 +35,6 @@ func failf(format string, v ...interface{}) {
 	os.Exit(1)
 }
 
-type WaitForBootCompleteResult struct {
-	Booted bool
-	Error  error
-}
-
-func getBootCompleteEvent(adbManager *adbmanager.Model, serial string, timeout time.Duration) <-chan WaitForBootCompleteResult {
-	doneChan := make(chan WaitForBootCompleteResult)
-
-	go func() {
-		time.AfterFunc(timeout, func() {
-			doneChan <- WaitForBootCompleteResult{Error: errors.New("timeout")}
-		})
-	}()
-
-	go func() {
-		cmd := adbManager.WaitForDeviceThenShellCmd(serial, nil, "getprop sys.boot_completed")
-		out, err := cmd.RunAndReturnTrimmedCombinedOutput()
-		if err != nil {
-			fmt.Println(cmd.PrintableCommandArgs())
-			fmt.Println(out)
-			doneChan <- WaitForBootCompleteResult{Error: err}
-			return
-		}
-
-		lines := strings.Split(out, "\n")
-		for _, line := range lines {
-			if strings.TrimSpace(line) == "1" {
-				doneChan <- WaitForBootCompleteResult{Booted: true}
-				return
-			}
-		}
-
-		doneChan <- WaitForBootCompleteResult{Booted: false}
-	}()
-
-	return doneChan
-}
-
-func waitForDevice(adb *adbmanager.Model, emulatorSerial string, timeout time.Duration) error {
-	startTime := clock.Now()
-
-	for {
-		logger.Printf("Waiting for emulator boot...")
-
-		bootCompleteChan := getBootCompleteEvent(adb, emulatorSerial, timeout)
-		result := <-bootCompleteChan
-		switch {
-		case result.Error != nil:
-			logger.Warnf("Failed to check emulator boot status: %s", result.Error)
-			logger.Warnf("Killing ADB server before retry...")
-			killCmd := adb.KillServerCmd(nil)
-			if out, err := killCmd.RunAndReturnTrimmedCombinedOutput(); err != nil {
-				return fmt.Errorf("failed to terminate adb server: %s", out)
-			}
-		case result.Booted:
-			logger.Donef("Device boot completed in %d seconds", time.Since(startTime)/time.Second)
-			return nil
-		}
-
-		if time.Now().After(startTime.Add(timeout)) {
-			return fmt.Errorf("emulator boot check timed out after %s seconds", time.Since(startTime)/time.Second)
-		}
-
-		delay := 5 * time.Second
-		logger.Printf("Device is online but still booting, retrying in %d seconds", delay/time.Second)
-		time.Sleep(delay)
-	}
-}
-
 func main() {
 	envRepo := env.NewRepository()
 	cmdFactory := command.NewFactory(env.NewRepository())
@@ -133,12 +52,12 @@ func main() {
 		failf("Failed to create Android SDK: %s", err)
 	}
 
-	adb, err := adbmanager.New(androidSdk, cmdFactory)
+	adb, err := adbmanager.New(androidSdk, cmdFactory, logger)
 	if err != nil {
 		failf("Failed to create ADB model: %s", err)
 	}
 
-	if err := waitForDevice(adb, inputs.EmulatorSerial, time.Duration(inputs.BootTimeout)*time.Second); err != nil {
+	if err := adb.WaitForDevice(inputs.EmulatorSerial, time.Duration(inputs.BootTimeout)*time.Second); err != nil {
 		failf(err.Error())
 	}
 
